7ca09e5b176a21c61d8cab03dff48c06e1dab68e
jacksonalves <jacksonalves@gmail.com>
2018-10-21 23:00:14 -0300

    implementado service de upload de image com s3 aws da amazon

--- src/main/java/com/jackson/cursomc/CursomcApplication.java
+++ src/main/java/com/jackson/cursomc/CursomcApplication.java
@@ -8,12 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CursomcApplication implements CommandLineRunner{
 	
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... arg0) throws Exception {	
-		
+	
 	}
 }
--- src/main/java/com/jackson/cursomc/services/ClienteService.java
+++ src/main/java/com/jackson/cursomc/services/ClienteService.java
@@ -1,9 +1,12 @@
 package com.jackson.cursomc.services;
 
+import java.awt.image.BufferedImage;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,6 +14,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.jackson.cursomc.domain.Cidade;
 import com.jackson.cursomc.domain.Cliente;
@@ -37,6 +41,18 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3Service;
+	
+	@Autowired
+	ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private int size;
 	
 	public Cliente find(Integer id) {
 		
@@ -105,5 +121,20 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		String fileName = prefix + user.getId() + "jpg";
+		
+		return s3Service.uplodFile(imageService.getImputStream(jpgImage, "jpg"), fileName, "image");
 	}
 }
